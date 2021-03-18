8d1ac2917a35b6bab3d38bf6ca4d0f5ead0a92f4
Adrien Husson <husson.adr@gmail.com>
2021-03-18 18:42:14 +0100

    init

--- DancingQueen_MaxProject/code/TrackTempoFollower.js
+++ DancingQueen_MaxProject/code/TrackTempoFollower.js
@@ -1,25 +1,46 @@
 inlets = 1;
-outlets = 1;
+outlets = 2;
 
 var trackTempo = 120.0; // bpm
-var rangeTempo = 5.0;   // bpm
+var rangeTempo = 5.0; // bpm
 
-function pushTempo(tempo_) {
-    var absDiff_ = Math.abs(trackTempo - tempo_);
-    var relDiff_ = 0; // likelyhood
+var minTempo = 40;
+var maxTempo = 220;
 
-    if(absDiff_ <= rangeTempo) {
-        relDiff_ = absDiff_ / rangeTempo;
-        relDiff_ = 1 - relDiff_;
+var tempoTracks = []; // ordered by user
+var orderTempos = [minTempo, maxTempo]; // ordered by tempo
+var orderOutput = []; // outputs ordered by tempo
+var outputs = []; // outputs ordered for user
+
+function pushCurTempo(tempo_) {
+    // for()
+
+  for (var i = 0; i < orderTempos.length - 1; i++) {
+    if (tempo_ >= orderTempos[i] && tempo_ < orderTempos[i + 1]) {
+
     }
+  }
 
-    outlet(0, relDiff_); // return likelyhood [0,1]
+  outlet(0, outputs); // return array of likelyhood
 }
 
-function setTrackTempo(tempo_) {
-    trackTempo = tempo_;
+function addTrackTempo(tempTracks_) {
+  // Add tempo array
+  tempoTracks.push(tempTracks_);
+
+  // Add to ordered tempo array
+  orderTempos = tempoTracks.sort(function (a, b) {
+    return a - b;
+  });
+  orderTempos.splice(0, 0, minTempo); // insert min tempo
+  orderTempos.push(maxTempo); // insert max tempo
+//   orderTempos = [...new Set(orderTempos)]; // remove double value
+
+  outlet(0, tempoTracks);
+  outlet(1, orderTempos);
 }
 
-function setRangeTempo(tempo_) {
-    rangeTempo = tempo_;
+function resetsdf() {
+    tempoTracks = [];
+    orderTempos = [minTempo, maxTempo];
 }
